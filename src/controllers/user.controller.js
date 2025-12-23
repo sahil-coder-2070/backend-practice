04942243c4758bcfc6db7b8b0b1ed1ad481c0b3b
Sahil Singh <sahil171759@gmail.com>
2025-12-23 23:22:04 +0530

    add register logic and fixed error

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,11 +2,23 @@ import asyncHandler from "../utils/asyncHandler.js"
 import ApiError from "../utils/ApiErr.js";
 import { User } from "../models/user.model.js";
 import uploadOnCloudinary from "../utils/Cloudinary.js";
-
+import ApiResponse from "../utils/ApiResponse.js";
 const registerUser = asyncHandler(async (req, res) => {
     // res.status(200).json({
     //     message: "server is running"
     // })
+
+    // get user details from frontend
+    // validation - not empty
+    // check if user already exists: username, email
+    // check for images, check for avatar
+    // upload them to cloudinary, avatar
+    // create user object - create entry in db
+    // remove password and refresh token field from response
+    // check for user creation
+    // return res
+
+
     const { email, fullname, username, password } = req.body
     console.log("email: ", email);
     if (
@@ -36,8 +48,37 @@ const registerUser = asyncHandler(async (req, res) => {
 
     // uploading on cloudinary
     const avatar = await uploadOnCloudinary(avatarLocalPath)
-    const coverImage6 = await uploadOnCloudinary(coverImageLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
+    if (!avatar) {
+        throw new ApiError(400, "Avatar required")
+    }
+
+    const user = await User.create({
+        fullname,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        email,
+        username,
+        password,
+    })
+
+    const createuser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if (!createuser) {
+        throw new ApiError(500, "Error while creating user")
+    }
+
+    return res.status(200, (
+        new ApiResponse(
+            200,
+            createuser,
+            "user registered successfully"
+
+        )
+    ))
 
 })
 
